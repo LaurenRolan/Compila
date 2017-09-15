3e60c921006b879849b3218934326ef275fa273a
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-09-15 18:04:33 -0300

    corrigindo um erro trouxa

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv){
   if(argc < 2) {
-    fprinf(stderr, "Missing file name.\n Call ./a.out file_name.\n");
+    fprintf(stderr, "Missing file name.\n Call ./a.out file_name.\n");
     exit(1);
   }
   return 0;
