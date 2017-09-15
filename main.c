349884e4f3c04187afb479f8f967b35060f24558
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-09-15 17:00:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(){
-  
+int main(int argc, char **argv){
+  if(argc < 2) {
+    fprinf(stderr, "Missing file name.\n Call ./a.out file_name.\n");
+    exit(1);
+  }
   return 0;
 }
