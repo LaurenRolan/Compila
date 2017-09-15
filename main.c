87f60a575d17f271273a10a495eba1e47300e8b2
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-09-15 19:47:58 -0300

    usando file

--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+  FILE *fh;
   if(argc < 2) {
     fprintf(stderr, "Missing file name.\n Call ./a.out file_name.\n");
     exit(1);
   }
+
+  if ((fh = fopen(argv[1], "r")))
+        yyin = fh;
   yylex();
   return 0;
 }
