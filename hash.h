3c03cd90424ab2a05afb90e482aa61791a72aa01
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-09-15 17:28:31 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,1 +1,12 @@
+#define HASH_SIZE 256
 
+typedef struct node {
+  struct node * next;
+  char *text;
+  int type;
+}HASH_NODE;
+
+void hashInit(void);
+int hashAddress(char *text);
+HASH_NODE *hashInsert(int type, char *text);
+void hashPrint(void);
