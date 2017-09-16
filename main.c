3e2de11bb247afac26f5944f463764ec87313cf1
Nicolas <nicolas.eymael@hotmail.com>
2017-09-16 16:02:54 -0300

    oi lauren tudo bem

--- main.c
+++ main.c
@@ -3,13 +3,24 @@
 
 int main(int argc, char **argv){
   FILE *fh;
+  int var=8;
   if(argc < 2) {
     fprintf(stderr, "Missing file name.\n Call ./a.out file_name.\n");
     exit(1);
   }
 
   if ((fh = fopen(argv[1], "r")))
+  {
         yyin = fh;
-  yylex();
+	  
+		while(var != 0)
+		{
+			var=yylex();
+      if(var!=0)
+			  fprintf(stderr,"O retorno eh %d\n",var);
+		}
+  }
+  else fprintf(stderr,"nao deu");
+	
   return 0;
 }
