86375953123369b59d05267f0040bfa9b816b98c
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-10-08 14:50:09 -0300

    add astree.h

---
+++ astree.h
@@ -0,0 +1,12 @@
+#include "hash.h"
+
+#define MAX_SONS 4
+
+typedef struct ast_node {
+  int type;
+  HASH_NODE symbol;
+  struct ast_node son[MAX_SONS];
+} AST;
+
+AST *astCreate(int type, HASH_NODE symbol, AST* son0, AST* son1, AST* son2, AST* son3);
+void astPrint(AST *node);
