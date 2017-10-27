c8f651ee1d9a2877c8368aa6a5064f91fa35d8af
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-10-27 10:18:33 -0200

    Aaaaaacho que corrigi.

--- astree.c
+++ astree.c
@@ -4,9 +4,10 @@
 
 AST *astCreate(int type, HASH_NODE *symbol, AST* son0, AST* son1, AST* son2, AST* son3){
   AST *newnode;
-  newnode = (AST*) calloc(1, sizeof(AST));
+  newnode = (AST*) malloc(sizeof(AST));
   newnode->type = type;
-  newnode->symbol = *symbol;
+  if (symbol != NULL)
+	newnode->symbol = *symbol;
   newnode->son[0] = son0;
   newnode->son[1] = son1;
   newnode->son[2] = son2;
