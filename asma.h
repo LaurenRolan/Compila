482d13d79c8ff6c01312f1b558f76c213f77c79d
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-12-14 17:25:34 -0200

    Create asma.h

---
+++ asma.h
@@ -0,0 +1,10 @@
+//Feito por 
+//Lauren Silva Rolan Sampaio
+//Nicolas Eymael da Silva
+#ifndef ASMA_HEADER
+#define ASMA_HEADER
+#include "tac.h"
+
+void asmGenerator(char *filename, TAC *code);
+
+#endif
