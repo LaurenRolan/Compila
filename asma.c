a2ef99f4ea29c0d0ccfd70f3ba596cf3685bc32c
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-12-14 20:21:53 -0200

    pra ti te divertir

--- asma.c
+++ asma.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Protótipos internos
+void makeAdd(TAC *tac, FILE *fout);
+void makeSub(TAC *tac, FILE *fout);
+void makeMul(TAC *tac, FILE *fout);
+void makeDiv(TAC *tac, FILE *fout);
+void makeAss(TAC *tac, FILE *fout);
+void makeOR(TAC *tac, FILE *fout);
+void makeAND(TAC *tac, FILE *fout);
+void makeGRE(TAC *tac, FILE *fout);
+void makeLES(TAC *tac, FILE *fout);
+void makeGEQ(TAC *tac, FILE *fout);
+void makeLEQ(TAC *tac, FILE *fout);
+void makeAss(TAC *tac, FILE *fout);
+void makePrint(TAC *tac, FILE *fout);
+void makeRead(TAC *tac, FILE *fout);
+void makeWhile(TAC *tac, FILE *fout);
+void makeIfElse(TAC *tac, FILE *fout);
+//Fim dos protótipos internos
 void asmGenerator (char *filename, TAC *code)
 {
 	FILE *fout;
@@ -15,15 +33,88 @@ void asmGenerator (char *filename, TAC *code)
 	{
 		switch(tac->type)
 		{
-			case TAC_ADD: fprintf(fout, "\nmovq\t %s(%%rip), %%rdx\n"
-					      "movq\t %s(%%rip), %%rax\n"
-					      "addq\t %%rdx, %%rax\n", tac->op1->text, tac->op2->text); break;
-			case TAC_PRINT: fprintf(fout, "\nmovq\t %s(%%rip), %%rax\n"
-						"movq\t %%rax, %%rsi\n"
-						"movl\t$printable, %%edi\n"
-						"movl\t$0, %%eax\n"
-						"call\tprintf\n", tac->res->text); break;
+			case TAC_ADD: makeAdd(tac, fout); break;
+			case TAC_SUB: makeSub(tac, fout); break;
+			case TAC_DIV: makeDiv(tac, fout); break;
+			case TAC_MUL: makeMul(tac, fout); break;
+			case TAC_ASS: makeAss(tac, fout); break;
+			case TAC_PRINT: 
 			
 		}
 	}
+}
+
+void makeAdd(TAC* tac, FILE *fout)
+{
+	//Testar se é lit + lit, lit + var ou var + var
+	fprintf(fout, "\nmovq\t %s(%%rip), %%rdx\n"
+		"movq\t %s(%%rip), %%rax\n"
+		"addq\t %%rdx, %%rax\n", tac->op1->text, tac->op2->text); break;
+}
+void makeSub(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makeMul(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makeDiv(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makeOR(TAC *tac, FILE *fout)	
+{
+	
+}
+void makeAND(TAC *tac, FILE *fout)
+{
+	
+}
+void makeGRE(TAC *tac, FILE *fout)
+{
+	
+}
+void makeLES(TAC *tac, FILE *fout)
+{
+	
+}
+void makeGEQ(TAC *tac, FILE *fout)
+{
+	
+}
+void makeLEQ(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makeAss(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makePrint(TAC *tac, FILE *fout)
+{
+	fprintf(fout, "\nmovq\t %s(%%rip), %%rax\n"
+		"movq\t %%rax, %%rsi\n"
+		"movl\t$printable, %%edi\n"
+		"movl\t$0, %%eax\n"
+		"call\tprintf\n", tac->res->text); break;
+}
+
+void makeRead(TAC *tac, FILE *fout)
+{
+	
+}
+
+void makeWhile(TAC *tac, FILE *fout)
+{
+	
+}
+void makeIfElse(TAC *tac, FILE *fout)
+{
+	
 }
