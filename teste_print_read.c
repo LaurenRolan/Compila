47dcea450854b3c686704c5fa4d1998811b1e7df
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-12-14 18:39:52 -0200

    Create teste_print_read.c

---
+++ teste_print_read.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long a=111, b=222, c=333;
+
+int main()
+{
+  printf("Nao");
+  printf("a = %ld e b = %ld", a, b);
+  printf("%ld %ld %ld", a, b, c);
+  scanf("%ld", &a);
+}
