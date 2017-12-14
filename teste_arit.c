518c7ce47a06483cdd968222073970e32a914ac6
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-12-14 18:30:53 -0200

    Create teste_arit.c

---
+++ teste_arit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long a, b, c;
+double x, y, z;
+
+int main()
+{
+  a = 5;
+  b = 3;
+  c = a + b;
+  b = a - c;
+  x = 1.2 * 3.5;
+  y = x / 3.0;
+  if(a >= y) z = y * x;
+  if(a <= y) z = y * x;
+  if(a > y) z = y * x;
+  if(a < y) z = y * x;
+  if(a == y) z = y * x;
+  if (a && b) z = y * x;
+  if(a || b) z = y * x;
+}
